922d776f72a5e8615c4eb02437ce057bbb416ead
JonasPKnochelmann <jonas.p.knochelmann@gmail.com>
2022-05-31 12:56:50 -0400

    Started work on simple functional parser

---
+++ BroncoParser/Result.cs
@@ -0,0 +1,23 @@
+﻿namespace BroncoParser
+{
+    public class Result<T>
+    {
+        public T Value { get; private set; }
+
+        public bool Success { get; private set; }
+
+        public string Remainder { get; private set; }
+
+        public Result(T value, string remainder)
+        {
+            Value = value;
+            Success = true;
+            Remainder = remainder;
+        }
+
+        public Result()
+        {
+            Success = false;
+        }
+    }
+}
