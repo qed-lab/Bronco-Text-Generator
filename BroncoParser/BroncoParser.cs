7d3220bfc041b3b2b7e0d0fac373b2fd1066472a
JonasPKnochelmann <jonas.p.knochelmann@gmail.com>
2022-05-31 17:27:52 -0400

    Gave up on trying to build a parsing library from scratch, installed Sprache, started working with it

---
+++ BroncoParser/BroncoParser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BroncoLibrary;
+using Sprache;
+
+namespace BroncoParser
+{
+    internal static class BroncoParser
+    {
+        private static string nameChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_";
+
+        public static readonly Dictionary<String, ISymbol> SymbolReferences = new Dictionary<string, ISymbol>();
+
+        public static readonly Parser<ISymbol> NonTerminal =
+            from openBrace in Parse.Char('<').Once()
+            from reference in Parse.Chars(nameChars).Many().Text()
+            from closeBrace in Parse.Char('>').Once()
+            select SymbolReferences[reference];
+
+        public static readonly Parser<Terminal> Terminal =
+            from text in
+            (from not in Parse.Not(NonTerminal)
+             from c in Parse.CharExcept('\n')
+             select c).Many().Text()
+            select new Terminal(text);
+
+        public static readonly Parser<string> bagTitle =
+            from openBrace in Parse.Char('<').Once()
+            from title in Parse.Chars(nameChars).Many().Text()
+            from closeBrace in Parse.Char('>').Once()
+            select title;
+    }
+}
