6952ef36d4313708e260382a155fa78bcf695185
JonasPKnochelmann <jonas.p.knochelmann@gmail.com>
2022-06-08 10:37:32 -0600

    Cleaned up file organization a lot. Started work on better tags.

---
+++ BroncoLibrary/Primative Symbols/FloatSymbol.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BroncoLibrary
+{
+    internal class FloatSymbol : ITerminal
+    {
+        public float FloatValue { get; set; }
+
+        public string Value { get => FloatValue.ToString(); }
+
+        public FloatSymbol(float value)
+        {
+            FloatValue= value;
+        }
+    }
+}
