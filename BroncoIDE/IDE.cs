420753553a84746caf74f24f344fa5ae99e5b706
JonasPKnochelmann <jonas.p.knochelmann@gmail.com>
2022-06-27 16:14:09 -0600

    Modified syntax highlighting a small bit

--- BroncoIDE/IDE.cs
+++ BroncoIDE/IDE.cs
@@ -11,6 +11,7 @@ namespace BroncoIDE
         private static TextStyle blue = new TextStyle(Brushes.LightBlue, null, style);
         private static TextStyle red = new TextStyle(Brushes.Red, null, style);
         private static TextStyle green = new TextStyle(Brushes.Green, null, style);
+        private static TextStyle grey = new TextStyle(Brushes.Gray, null, style);
 
         public IDE()
         {
@@ -44,15 +45,16 @@ namespace BroncoIDE
             BackgroundParser.RunWorkerAsync(inputPane.Text);
             generateButton.Enabled = false;
         }
-
+        
         private void inputPane_TextChanged(object sender, FastColoredTextBoxNS.TextChangedEventArgs e)
         {
             e.ChangedRange.ClearStyle(blue, red, green);
             e.ChangedRange.SetStyle(blue, "#[A-Za-z][A-Za-z0-9]*");
             e.ChangedRange.SetStyle(blue, "#[A-Za-z][A-Za-z0-9]*:[0-9.]*");
             e.ChangedRange.SetStyle(blue, "%[0-9.]*");
-            e.ChangedRange.SetStyle(red, "[<>]");
+            e.ChangedRange.SetStyle(red, "<.*?>");
             e.ChangedRange.SetStyle(green, "@[A-Za-z][A-Za-z0-9]*");
+            e.ChangedRange.SetStyle(grey, "/\\*.*?\\*/");
         }
     }
 }
